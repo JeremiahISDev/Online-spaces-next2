4c74851cd1ace64f7af2b276aa08f06e21627438
Jeremiah Ihab Samuel <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-01-19 21:29:38 +0000

    5

--- pages/SignUp.jsx
+++ pages/SignUp.jsx
@@ -42,13 +42,6 @@ function SignUp() {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email Address"
-                  ref={{
-                    register({
-                      required: "Email is required.",
-                      pattern: {
-                        value: ""/^ [a - z0 -9._ % +-] +@[a-z0-9.-]+\.[a-z]{2,4}$/",
-                message: "Please enter a valid email"
-                }}}}}
               />
               </div>
               {errors.email && <ErrorMessage message={errors.email.message} />}
