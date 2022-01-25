144c7af13cede1fac279cb707935f457494dc407
Jeremiah Ihab Samuel <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-01-25 22:59:10 +0000

    fix

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -6,13 +6,13 @@ const Navigation = () => {
 
   return (
     <Head>
-    <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
-<link rel="manifest" href="/assets/site.webmanifest">
-<link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#8800ff">
-<meta name="msapplication-TileColor" content="#603cba">
-<meta name="theme-color" content="#0088ff">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png"/>
+<link rel="manifest" href="/assets/site.webmanifest"/>
+<link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#8800ff"/>
+<meta name="msapplication-TileColor" content="#603cba"/>
+<meta name="theme-color" content="#0088ff"/>
     </Head>
     <header className="flex items-center p-3 flex-wrap text-white bg-purple-700">
       <div
