feb280a085abb3bde3880c55c1de0aace645b80f
Jeremiah Ihab Samuel <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-01-25 23:00:14 +0000

    fix2

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -5,6 +5,7 @@ const Navigation = () => {
   const [showNav, setShowNav] = useState(false);
 
   return (
+    <>
     <Head>
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png"/>
 <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png"/>
@@ -76,6 +77,7 @@ const Navigation = () => {
       </div>
       
     </header>
+    </>
   );
 };
 
