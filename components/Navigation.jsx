456b3df87cc7ce385dd4aecb174748e98455b23a
Jeremiah Ihab Samuel <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-01-25 01:18:13 +0000

    v

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,45 +1,73 @@
-import Link from "next/link"
-function Navigation() {
-	return (
-		<div>
-			<nav className="bg-white dark:bg-gray-800  shadow ">
-				<div className="max-w-7xl mx-auto px-8">
-					<div className="flex items-center justify-between h-16">
-						<div className="w-full justify-between flex items-center">
-							<Link href="/"><a className="flex-shrink-0">
-								<img className="h-12 w-12" src="/assets/Logo.png" alt="Online Spaces" />
-							</a></Link>
-							<div className="hidden md:block">
-								<div className="ml-10 flex items-baseline space-x-4">
-									<Link href="/"><a className="text-gray-800  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-										Home
-                            </a></Link>
-									<Link href="/Pricing"><a className="text-gray-800 dark:text-white  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-										Pricing
-                            </a></Link>
-									<Link href="/Contact"><a className="text-gray-800  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-										Contact
-                            </a></Link>
-									<Link href="/About"><a className="text-gray-800  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-										About Us
-                            </a></Link>
-									<Link href="/SignUp"><a className="text-gray-800  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-										<button className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200">
-											Try One Free Meeting!
-                                </button>
-									</a></Link>
-								</div>
-							</div>
-						</div>
-						<div className="block">
-							<div className="ml-4 flex items-center md:ml-6">
-							</div>
-						</div>
-					</div>
-				</div>
-			</nav>
-		</div>
+import { useState } from "react";
+import Link from "next/link";
 
-	)
-}
+const Navigation = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <header className="flex items-center p-3 flex-wrap text-white bg-purple-700">
+      <div
+        id="logo"
+        className="lg:text-xl p-2 mr-4 inline-flex items-center font-serif font-bold"
+      >
+        <Link href="/">
+        <span>
+        <img className="h-10 w-10" src="/assets/Logo.png" alt="Online Spaces" />
+        </span>
+        </Link>
+      </div>
+      <button
+        onClick={() => setShowNav(!showNav)}
+        type="button"
+        className="inline-flex p-3 text-white hover:text-gray-300 focus:text-white focus:outline-none lg:hidden ml-auto"
+      >
+        <svg
+          className="h-6 w-6 fill-current"
+          viewBox="0 -53 384 384"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
+          <path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
+          <path d="m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
+        </svg>
+      </button>
+
+      <div className="w-full flex-grow lg:inline-flex lg:flex-grow lg:w-auto">
+        <div
+          className={
+            "lg:inline-flex lg:flex-row lg:ml-auto flex flex-col " +
+            (showNav ? "" : "hidden")
+          }
+        >
+          <Link href="/">
+            <a className="lg:inline-flex lg:w-auto px-3 py-2 rounded hover:bg-blue-600">
+              Home
+            </a>
+          </Link>
+
+          <Link href="/About">
+            <a className="lg:inline-flex lg:w-auto px-3 py-2 rounded hover:bg-blue-600">
+              About
+            </a>
+          </Link>
+          <Link href="/Contact">
+            <a className="lg:inline-flex lg:w-auto px-3 py-2 rounded hover:bg-blue-600">
+              Contact Us
+            </a>
+          </Link>
+          <Link href="/Pricing">
+            <a className="lg:inline-flex lg:w-auto px-3 py-2 rounded hover:bg-blue-600">
+              Pricing
+            </a>
+          </Link>
+        <Link href="/SignUp"><button className="text-white ml-2 bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 rounded-full text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 font-bold">
+        Try One Free Meeting!
+        </button></Link>
+        </div>
+      </div>
+      
+    </header>
+  );
+};
+
 export default Navigation;
--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -5,10 +5,20 @@ function Contact() {
     <div id="Title" className="text-center text-blue-600 font-bold text-[2rem] pb-6">
       <h1>Contact Us</h1>
       </div>
-      <div className="text-center" id="Form-Div">
-      <form className="text-center border-2 p-6 w-fit">
-        <label htmlFor="email" className="pr-4">Email:</label>
-        <input type="email" name="email" className="border-2 rounded p-2 border-blue-800" placeholder="example@domain.com"/>
+      <div className="text-center align-center align-items-center justify-center justify-items-center" id="Form-Div">
+      <form method="POST" className="text-center border-2 p-6 w-full">
+        <label htmlFor="email" className="px-4">Email:</label>
+        <input type="email" name="email" className="border-2 rounded p-2 mb-4 border-blue-800" placeholder="example@domain.com"/> <br/>
+        <label htmlFor="name" className="px-4">Name:</label>
+        <input type="text" name="name" className="border-2 rounded p-2 border-blue-800" placeholder="John Doe"/>
+        <br/>
+        <div className="pt-4">
+        <label htmlFor="message" className="px-4">Message:</label>
+        <textarea name="message" className="form-textarea mt-1 block w-full border-2 rounded p-2 border-blue-800" rows="3" placeholder="Message"></textarea>
+        </div>
+        <br/>
+        <br/>
+        <button type="submit" name="submit" className="h-10 px-5 text-blue-100 transition-colors duration-150 bg-blue-600 rounded-lg focus:shadow-outline hover:bg-blue-700 rounded-full">Submit</button>
       </form>
       </div>
     </div>
