04fdec672a23df929d64ae57269277fc87b7b24e
JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-03-30 20:46:28 -0400

    Finish about
    Signed-off-by: JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>

--- pages/About.jsx
+++ pages/About.jsx
@@ -7,9 +7,10 @@ function About() {
             <div className="w-fit border-2 m-4 p-6 border-blue-700 rounded overflow-hidden shadow-lg">
         <p className="text-center text-blue-800 font-extrabold"> 
           <div><a href="./assets/Checklist.pdf" download="Project_Checklist_Final.pdf">Checklist</a></div>
+          <div><a href="./assets/Log.pdf" download="Project_Worklog_Final.pdf">Worklog</a></div>
           <div className="text-black font-normal">This website was built
 using TailwindCSS. The overall layout, structure, and design of the website are
-original and were developed from scratch by members of team ######.</div>
+original and were developed from scratch by members of team 1067.</div>
             </p>
             </div>
           </div>
